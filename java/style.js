e6f1071873e8cf7cce67eccdbb671332f15bb537
¨belenkf¨ <belenkf@gmail.com>
2022-07-14 19:03:54 -0300

    pruebajavaconsole

--- java/style.js
+++ java/style.js
@@ -1,11 +1,45 @@
+
+
+const camilo ={
+    nombre: ¨camilo¨
+    edad: 22,
+    sexo: ¨masculino¨,
+    pasatiempos:[¨patinar¨, ¨bailar¨],
+    hablar: function(){
+        return ¨hola soy $ { this.nombre }, y tengo $ { this.edad} años¨;
+     };
+    }
+console.log (camilo);
+
+function Persona (nombre, edad, sexo, pasatiempos){
+this.nombre = nombre;
+this.edad = edad; 
+this.sexo = sexo;
+this.pasatiempos = pasatiempos;
+this.hablar = function () {
+return ¨hola soy $ {this.nombre}, y tengo $ {this.edad} años¨;
+
+};
+}
+
+( )
+[]
+{}
 document.write("belu");
+
+
+
+
 // boolean
 
 //array
 
 //object
 
-var Enviar = "Enviado"
+//info date
+
+
+consol.log var Enviar = "Enviado"
 function send (){
     
     console.log(Enviar)
