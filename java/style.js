e3c19cb29f9870007d7b4c9b5f3db86d4ad30283
¨belenkf¨ <belenkf@gmail.com>
2022-07-14 00:57:27 -0300

    java

---
+++ java/style.js
@@ -0,0 +1,15 @@
+document.write("belu");
+// boolean
+
+//array
+
+//object
+
+var Enviar = "Enviado"
+function send (){
+    
+    console.log(Enviar)
+}
+console.log(Enviar)
+cancelar()
+console.log(Enviar)
