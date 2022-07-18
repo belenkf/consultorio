232a31dbd4ce698093e6e76001ac1cd241635ad1
¨belenkf¨ <belenkf@gmail.com>
2022-07-17 23:30:39 -0300

    probandobotonesjava

--- java/style.js
+++ java/style.js
@@ -1,49 +1,7 @@
 
+// btn btn-outline-success; window.alert (message);alert ("cual es tu nombre");
+// index
 
-const camilo ={
-    nombre: ¨camilo¨
-    edad: 22,
-    sexo: ¨masculino¨,
-    pasatiempos:[¨patinar¨, ¨bailar¨],
-    hablar: function(){
-        return ¨hola soy $ { this.nombre }, y tengo $ { this.edad} años¨;
-     };
-    }
-console.log (camilo);
+//pruebajava
 
-function Persona (nombre, edad, sexo, pasatiempos){
-this.nombre = nombre;
-this.edad = edad; 
-this.sexo = sexo;
-this.pasatiempos = pasatiempos;
-this.hablar = function () {
-return ¨hola soy $ {this.nombre}, y tengo $ {this.edad} años¨;
-
-};
-}
-
-( )
-[]
-{}
-document.write("belu");
-
-
-
-
-// boolean
-
-//array
-
-//object
-
-//info date
-
-
-consol.log var Enviar = "Enviado"
-function send (){
-    
-    console.log(Enviar)
-}
-console.log(Enviar)
-cancelar()
-console.log(Enviar)
+function
