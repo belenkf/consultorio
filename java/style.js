e107af28abb668522796bf14ebf1ae941b393fa8
¨belenkf¨ <belenkf@gmail.com>
2022-07-26 18:29:21 -0300

    javaa

--- java/style.js
+++
@@ -1,6 +0,0 @@
-
-// btn btn-outline-success; window.alert (message);alert ("cual es tu nombre");
-// index
-
-//pruebajava
-
---
+++ js/style.js
@@ -0,0 +1,41 @@
+
+// btn btn-outline-success; window.alert (message);alert ("cual es tu nombre");
+// index
+
+//pruebajava
+function consulta() {
+    
+    var nombre = new consulta();
+    
+    msg = 'El saldo de consultas es de: ' + consulta1.saldo + ' sesiones';
+    
+    alert (msg);
+    
+    consulta1.mostrarDatos();
+    
+    }
+
+function turno () {
+
+    this.nombre = "";
+    
+    this.apellido = "";
+    
+    this.telefono = "";
+    
+    this.mostrarDatos = function () {
+    
+                    var msg = "Gracias"  +  nombre  +  apellido + telefono + "nos pondremos en contacto contigo para chequear un próximo turno disponible";;
+    
+                    msg = msg;
+    
+                    alert(msg);
+    
+                    }
+    
+    }
+    
+     
+    
+  
+    
